fa3eed6b0e2003656c0e64bcba4430521bc72abb
B Pope <poper38@gmail.com>
2020-07-20 23:28:54 -0400

    summarized each make/model/year by number of recalls; next step is to model the exponential distribution on number of recalls

--- app.R
+++ app.R
@@ -13,6 +13,9 @@ library(tidyverse)
 library(jsonlite)
 library(httr)
 library(shinyWidgets)
+library(ggplot2)
+library(vcd)
+library(MASS)
 
 #hard coded column names and descriptions
 txt_col<-c('RECORD_ID','CAMPNO','MAKETXT','MODELTXT',
@@ -58,30 +61,104 @@ ui <- dashboardPage(
                         "Filter on Make:",
                         choices=makes,
                         selected = makes,
-                        multiple=T)
+                        multiple=T,
+                        options = list(`actions-box`=T)),
+        uiOutput('model_box'),
+        uiOutput('year_box'),
+        checkboxGroupButtons("level",
+                             "Select Level of Summary:",
+                             choices=c("Make"="MAKETXT",
+                                       "Model"="MODELTXT",
+                                       "Year"="YEARTCT"))
         ),
     
         # show the meat and potatoes
         dashboardBody(
-           DT::dataTableOutput("table")
+            DT::dataTableOutput("table"),
+           plotOutput("test"),
+           plotOutput('cummulative')
         )
     
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-
+server <- function(input, output,session) {
+# Read in data. uncomment once out of development
     # temp <- tempfile()
     # download.file("https://www-odi.nhtsa.dot.gov/downloads/folders/Recalls/FLAT_RCL.zip",temp)
     # data <- read_delim(unz(temp, "FLAT_RCL.txt"),'\t',col_names = FALSE)
     # unlink(temp)
     # 
     # names(data)<-txt_col
-    
-    output$table <- DT::renderDataTable({
-        print(input$make_box)
-        DT::datatable(data %>% filter(MAKETXT %in% input$make_box) %>% distinct(MAKETXT) %>% arrange(MAKETXT))
+    # data$BGMAN<-as.Date(as.character(data$BGMAN),'%Y%m%d')
+    # data$RCDATE<-as.Date(as.character(data$RCDATE),'%Y%m%d')
+    # data$DATEA<-as.Date(as.character(data$DATEA),'%Y%m%d')
+
+    output$model_box<-renderUI({
+        data0<-(data %>% filter(MAKETXT %in% input$make_box) %>% distinct(MODELTXT))[[1]]
+        pickerInput('model_box','Filter on Model',
+                    choices=sort(data0),
+                    selected = sort(data0),
+                    multiple=T,
+                    options = list(`actions-box`=T))
     })
+    
+    
+        
+    output$year_box<-renderUI({
+        data1<-(data %>% filter(MAKETXT %in% input$make_box) %>% distinct(YEARTXT))[[1]]
+        pickerInput('year_box','Filter on Year',
+                    choices=sort(data1),
+                    selected = sort(data1),
+                    multiple=T,
+                    options = list(`actions-box`=T))
+    })
+    output$table <- DT::renderDataTable({
+        DT::datatable(data %>% 
+                          filter(MAKETXT %in% input$make_box,
+                                 MODELTXT %in% input$model_box,
+                                 YEARTXT %in% input$year_box) %>% 
+                          group_by(MAKETXT,MODELTXT,YEARTXT) %>% 
+                          summarise(recalls=n()) %>% 
+                          group_by(!!!syms(input$level)) %>% 
+                          summarise(recalls=sum(recalls,na.rm = T),
+                                divisor=n()) %>% 
+                          mutate(avg_recalls=recalls/divisor) %>% 
+                          dplyr::select(input$level,recalls,avg_recalls)
+                      )
+    })
+    
+    # notes: add average amount of recalls per make
+    # create predictive model for number of recalls
+    
+    pdata<-reactive({
+        data %>% mutate(days_since=as.integer(RCDATE-BGMAN)) %>% 
+            filter(MAKETXT %in% input$make_box, YEARTXT<=2010,
+                   days_since>0,days_since<7300) 
+        })
+    
+
+        ex<-as.integer(data$RCDATE-data$BGMAN)
+        ex<-ex[!is.na(ex)]
+        ex<-ex[ex>0]
+        fit1<-fitdistr(ex,"exponential")
+        # ks.test(x,"pexp",fit1$estimate)
+    
+    output$test<-renderPlot({
+        # hist(pdata()$days_since, freq = FALSE, breaks = 50)
+        # curve(dexp(x, rate = fit1$estimate), from = 0, col = "red", add = TRUE)
+        holder<-pdata()
+        holder$curve<-dexp(holder$days_since,rate=fit1$estimate)
+        ggplot(holder,aes(x=days_since)) +
+            geom_histogram(binwidth = 120,boundary=0,color="black",fill="grey") 
+            # geom_line(aes(y=curve))
+    })
+    output$cummulative<-renderPlot({
+        ggplot(pdata(),aes(x=days_since))+
+            geom_histogram(aes(y=cumsum(..count..)), binwidth = 120, boundary = 0,
+                           color = "black", fill = "grey")
+    })
+    
 }
 
 # Run the application 
